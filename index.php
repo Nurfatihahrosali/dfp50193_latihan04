ee04caaa1f1d8d1fa6f9d4592c9292b69660c886
Nurfatihahrosali <fatihaheren12@gmail.com>
2021-10-07 08:21:09 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+   define(JAWATAN,'Pengurus');
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Constant</title>
+</head>
+<body>
+    Saya, selaku <?php echo $jawatan; ?>
+    dengan ini melantik Encik Ahamd sebagai penolong <?php echo $jawatan; ?>
+</body>
+</html>
