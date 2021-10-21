20a6fbb75a2ad9753b146f8b13650d6d3ff43d7b
Nurfatihahrosali <fatihaheren12@gmail.com>
2021-10-21 10:54:24 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,12 @@
-<?php 
-   define(JAWATAN,'Pengurus');
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Constant</title>
+    <title>Document</title>
 </head>
 <body>
-    Saya, selaku <?php echo $jawatan; ?>
-    dengan ini melantik Encik Ahamd sebagai penolong <?php echo $jawatan; ?>
+    <h1></h1>
 </body>
 </html>
